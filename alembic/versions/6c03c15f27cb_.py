a273161e0adbfcb77c12a864050cba37a305571f
Mustafa Hassan <mustafa.hassan@stixor.com>
2025-09-28 21:13:55 +0500

    Add initial migration script for revision 6c03c15f27cb

---
+++ alembic/versions/6c03c15f27cb_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6c03c15f27cb
+Revises: 4f12ccccc42a, ed6dad71c9e5
+Create Date: 2025-09-28 21:13:42.061383
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6c03c15f27cb'
+down_revision = ('4f12ccccc42a', 'ed6dad71c9e5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
